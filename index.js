277482adf0073fd7e777ff2a089e92a50bf48d2b
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-06-16 07:40:19 +0300

    completed resubmission

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 // Code your solution here
-const drivers = ['Bobby' , 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-
-function findingMatch(array, name){
-    return array.filter((match) =>
-match.toLowerCase()=== name.toLowerCase())
-
-
-}
-let fuzzyMatch=(array, names)=>{
-    let fuzzy =[];
-    for(const name of array){
-        if (name.slice(0,1)===names.slice(0,1)){
-            fuzzy.push(name)
-        }
-    }
-    return fuzzy
-}
-const matchName = (array, str) => {
-    const matchl = [];
-    for (const user of array){
-        if(user.name.toLowerCase() === str.toLowerCase() || user. hometown.toLowerCase() === str.toLowerCase())[
-            matchl.push(user)
-        ]
-    }
-    return matchl;
-}
+function findMatching(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
